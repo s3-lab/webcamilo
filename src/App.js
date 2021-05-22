c02b4b65f2139748c1d0084dc9a4fd443e8b02e8
Sebastián <salamancasebastian312@gmail.com>
2021-05-22 18:42:22 -0300

    camila web inicia

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React  from 'react';
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+
+// pages
+
+import Home from "./pages/home";
+import Works from "./pages/works";
+import About from "./pages/about";
+import Blog from "./pages/blog";
+import Prints from "./pages/prints";
+import Contact from "./pages/contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home/>
+        </Route>
+        <Route path="/works">
+          <Works/>
+        </Route>
+        <Route path="/about">
+          <About/>
+        </Route>
+        <Route path="/blog">
+          <Blog/>
+        </Route>
+        <Route path="/prints">
+          <Prints/>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
